becfb14877844f5c738bee2134404ea8f66f21fe
Serghei Mangul <serghei@login2.(none)>
2019-07-27 10:50:11 -0700

    add script for leafcutter

---
+++ master.leafcutter.sh
@@ -0,0 +1,11 @@
+ls *bam | awk -F "bam" '{print $1}' >samples.txt
+
+
+while read line
+do
+
+echo "/u/home/s/serghei/code/genomics.hoffman2/run.leafcutter.bam2junc.sh ${line}.bam ${line}.junc" >run.${line}.sh
+
+qsub -cwd -V -N leafcutter -l h_data=8G,highp,time=04:00:00 run.${line}.sh
+
+done<samples.txt
---
+++ run.leafcutter.bam2junc.sh
@@ -0,0 +1,23 @@
+
+if [ $# -lt 2 ]
+    then
+    echo "********************************************************************"
+    echo "This script was written by Serghei Mangul"
+    echo "********************************************************************"
+    echo ""
+    echo "1 <input1>   - bam file"
+    echo "2 <outdir>  - out junc file"
+    echo "--------------------------------------"
+    exit 1
+    fi
+
+
+tool='/u/home/s/serghei//code/import/leafcutter/scripts/bam2junc.sh' # channge it if location where leafcutter is installed is different 
+
+
+bam=$1
+junc=$2
+
+$tool $bam $junc
+ 
+
