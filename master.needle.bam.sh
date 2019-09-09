ca7002141d2524327450d3cbaea541d0be639ac6
Serghei Mangul <serghei@login1.(none)>
2019-09-09 13:29:55 -0700

    add raw data

--- master.needle.bam.sh
+++ master.needle.bam.sh
@@ -1,10 +1,10 @@
-ls *.fastq | awk -F ".fastq$" '{print $1}' >samples.txt
+ls *.bam | awk -F ".bam" '{print $1}' >samples.txt
 
 
 while read line
 do
 
-echo "/u/home/s/serghei/project/code/needle/needle.sh -fastq $PWD/${line}.fastq $PWD/${line}">run.${line}.sh
+echo "/u/home/s/serghei/project/code/needle/needle.sh $PWD/${line}.bam $PWD/${line}">run.${line}.sh
 #qsub -cwd -V -N needle -l h_data=16G,highp,time=24:00:00 run.${line}.sh
 
 done<samples.txt
