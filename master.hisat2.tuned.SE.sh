a431418515aa0941d3f07726cbbd618b9e64265d
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-10-15 17:17:46 -0700

    created master script for single end (SE) bulk RNA seq

---
+++ master.hisat2.tuned.SE.sh
@@ -0,0 +1,17 @@
+ls *_R1_001.fastq.gz | awk -F "_R1_001.fastq.gz" '{print $1}'>samples.txt
+
+ 
+
+
+while read line
+do
+
+
+echo "/u/home/s/serghei/code/genomics.hoffman2/run.hisat2.tuned.SE.sh ${line}_R1_001.fastq.gz ${line}">run.${line}.sh
+qsub -cwd -V -N map -l h_data=12G,highp,time=24:00:00 run.${line}.sh
+
+
+done<samples.txt
+
+
+	
