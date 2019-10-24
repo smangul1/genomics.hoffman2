b9263eb650a9e508fc97823a6a009a1db119b336
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-10-23 19:04:11 -0700

    update salmon path

--- master.salmon.sh
+++ master.salmon.sh
@@ -4,8 +4,9 @@ ls *_R1_001.fastq.gz | awk -F "_R1_001.fastq.gz" '{print $1}' >samples.txt
 while read line
 do
 
-echo "~/project/anaconda2/bin/salmon quant -i ~/project/salmon.db/gencode.v27.transcripts -l A -1 ${line}_R1_001.fastq.gz -2 ${line}_R2_001.fastq.gz -p 8 -o ${line}">run.${line}.sh
+echo "/u/home/s/serghei/project/anaconda2/bin/salmon quant -i /u/home/s/serghei/project/salmon.db/gencode.v27.transcripts -l A -1 ${line}_R1_001.fastq.gz -2 ${line}_R2_001.fastq.gz -p 8 -o ${line}">run.${line}.sh
 
 qsub -cwd -V -N salmon -l h_data=16G,highp,time=24:00:00 run.${line}.sh
 
 done<samples.txt
+
