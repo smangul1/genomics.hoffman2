0a744a40fc6852a054b515b76a2c2a2aeac614ec
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-10-23 17:52:07 -0700

    created leafcutter.star master and run scripts

---
+++ master.STAR.leafcutter.sh
@@ -0,0 +1,15 @@
+
+ls *_R1_001.fastq.gz | awk -F "_R1_001.fastq.gz" '{print $1}'>samples.txt
+
+ 
+
+while read line
+do
+
+pwd=$(pwd)
+
+echo "/u/home/s/serghei/code/genomics.hoffman2/run.STAR.leafcutter.sh ${pwd}${line}_R1_001.fastq.gz ${pwd}${line}_R2_001.fastq.gz ${line}.STAR.BAM">run.star.leafcutter.${line}.sh
+qsub -cwd -V -N map -l h_data=12G,highp,time=24:00:00 run.star.leafcutter.${line}.sh
+
+
+done<samples.txt
