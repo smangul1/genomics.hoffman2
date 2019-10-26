096c2188ddfa1733172d622976f8a7ce0007e704
Serghei Mangul <serghei@login2.(none)>
2019-10-25 22:28:07 -0700

    change to qsub array

--- master.STAR.leafcutter.sh
+++ master.STAR.leafcutter.sh
@@ -10,7 +10,9 @@ do
 pwd=$(pwd)
 
 echo "/u/home/s/serghei/code/genomics.hoffman2/run.STAR.leafcutter.sh ${pwd}/${line}_R1_001.fastq.gz ${pwd}/${line}_R2_001.fastq.gz ${line}.STAR.BAM">run.star.leafcutter.${line}.sh
-qsub -cwd -V -N map -l h_data=16G,highp,time=24:00:00 run.star.leafcutter.${line}.sh
+#qsub -cwd -V -N map -l h_data=16G,highp,time=24:00:00 run.star.leafcutter.${line}.sh
 
 
 done<samples.txt
+
+~/code/miscellaneous.scripts/submit_QSUB_array.sh  16 24
