5e705f058cf2eb809d56b244b6b036d37fc2e577
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-10-26 12:13:48 -0700

    changed imrep EO job name

--- master.imrep.bam.extendedOutput.sh
+++ master.imrep.bam.extendedOutput.sh
@@ -5,6 +5,6 @@ do
 
 echo "/u/home/s/serghei/project/anaconda2/bin/python /u/home/s/serghei/project/code/seeing.beyond.target/tools/imrep/imrep.py --extendedOutput --bam  --noOverlapStep --noCast ${line}.bam ${line}.cdr3" >run.${line}.sh
 
-qsub -cwd -V -N imrep -l h_data=16G,highp,time=24:00:00 run.${line}.sh
+qsub -cwd -V -N imrep_extended_output -l h_data=16G,highp,time=24:00:00 run.${line}.sh
 
 done<samples.txt
