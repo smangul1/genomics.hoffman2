e67c79292e36ee8832039fa61c5ec4fa09c4eee7
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-10-27 18:41:27 -0700

    helper message

--- master.imrep.compare.all.repertoires.sh
+++ master.imrep.compare.all.repertoires.sh
@@ -4,6 +4,7 @@
 module load python/2.7
 #above line is necessary to avoid compile vs runtime error of different python versions since serghei's python is version 2.7 and default hoffman is 2.6
 
+echo "only works if current directory contains only the output directories of clonality.py. Current dir can have other files but no other directories."
 
 echo "/u/home/s/serghei/project/anaconda2/bin/python /u/home/m/maxmellb/imrep_USC_fork/imrep/run.compare.repertoires.py $PWD/ ../imrep.pairwise.comparison/" > run.compare.ALL.repertoires.sh
 qsub -cwd -V -N compare_ALL_repertoires -l h_data=16G,highp,time=24:00:00 run.compare.ALL.repertoires.sh
