d1bd23dd82d3d2202697b55dd20dafbe72e6548a
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-11-05 15:06:14 -0800

    bug

--- master.sbt_microbiome.sh
+++ master.sbt_microbiome.sh
@@ -1,9 +1,9 @@
-ls *fastq | awk -F ".fastq" '{print $1}' >samples.txt
+ls *fastq | awk -F "sort_extended_unmapped.fastq" '{print $1}' >samples.txt
 
 while read line
 do
 
-echo "/u/home/s/serghei/code/sbt_tool/sbt_microbiome.sh ${line}.fastq ${line}" >run.${line}.sh
+echo "/u/home/s/serghei/code/sbt_tool/sbt_microbiome.sh ${line}.sort_extended_unmapped.fastq ${line}.sort_extended_unmapped" >run.${line}.sh
 
 qsub -cwd -V -N microbiome -l h_data=16G,highp,time=24:00:00 run.${line}.sh
 
