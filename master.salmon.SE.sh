90cc5d6ffb22a326389bbfb621a10c941f90f275
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-11-06 13:32:22 -0800

    salmon single end

---
+++ master.salmon.SE.sh
@@ -0,0 +1,10 @@
+ls *_R1_001.fastq.gz | awk -F "_R1_001.fastq.gz" '{print $1}' >samples.txt
+
+while read line
+do
+
+echo "/u/home/s/serghei/project/anaconda2/bin/salmon quant -i /u/home/s/serghei/project/salmon.db/gencode.v27.transcripts -l A -r ${line}_R1_001.fastq.gz -p 8 -o ${line}">run.${line}.sh
+
+qsub -cwd -V -N salmon -l h_data=16G,highp,time=24:00:00 run.${line}.sh
+
+done<samples.txt
