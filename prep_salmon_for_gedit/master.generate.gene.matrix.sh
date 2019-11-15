c57dcf6aa53050c59bed4e12868aeadace0cb655
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-11-15 14:57:26 -0800

    updated instructions

--- prep_salmon_for_gedit/master.generate.gene.matrix.sh
+++ prep_salmon_for_gedit/master.generate.gene.matrix.sh
@@ -16,5 +16,7 @@ qsub -cwd -V -N generate_gene_matrix -l h_data=16G,highp,time=24:00:00 run.gener
 
 done<samples.txt
 
-# run this file in same level as salmon output directories. It will go inside each directory and generate a gene matrix csv for each quant file.
-# Then run the master.merge.gene.matrices.sh script to merge the csv files into one complete gene matrix.
+# run this file first in same level as salmon output directories. It will go inside each directory and generate a gene matrix csv for each quant file. 
+# the individual gene matrices will be located inside the directory called gene_matrices
+# Then run the master.merge.gene.matrices.sh script in the same location as this script to merge the csv files into one complete gene matrix.
+# The final merged csv file gene matrix will be located inside of gene_matrices and called merged_gene_matrix.csv
--- prep_salmon_for_gedit/master.merge.gene.matrices.sh
+++ prep_salmon_for_gedit/master.merge.gene.matrices.sh
@@ -10,5 +10,5 @@ chmod u+x run.merge.sh
 qsub -cwd -V -N merge_matrices -l h_data=16G,highp,time=24:00:00 run.merge.sh
 
 
-# run this file inside of the gene_matrices directory. Should be located in same level as salmon output directories
-# you should see all the csv files generated from generage gene matrix.
+# After running master.generate.gene.matrix.sh, run this script in the same location to merge the csv files into one complete gene matrix.
+# The final merged csv file gene matrix will be located inside of gene_matrices and called merged_gene_matrix.csv
