4632e7d396c3b74c57624c0ece707a16bfd9457d
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-12-05 15:56:49 -0800

    notes for clonality

--- master.clonality.sh
+++ master.clonality.sh
@@ -13,3 +13,9 @@ qsub -cwd -V -N clonality -l h_data=16G,highp,time=24:00:00 run.${line}.sh
 
 done<samples.txt
 
+
+
+#command to combine the summary.cdr3.txt files into one csv for all clonality samples.
+#echo "SAMPLE,nIGH,nIGK,nIGL,nTCRA,nTCRB,nTCRD,nTCRG,loadIGH,loadIGK,loadIGL,loadTCRA,loadTCRB,loadTCRD,loadTCRG,alphaIGH,alphaIGK,alphaIGL,alphaTCRA,alphaTCRB,alphaTCRD,alphaTCRG" > combined_clonality.csv
+#tail -n +2 -q */*summary.cdr3.txt >> combined_clonality.csv
+
