2252b57e8dd6f89543c2879e2d75a4ce63455ebf
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-12-05 20:16:15 -0800

    rename for paired end reads

--- master.STAR.leafcutter.PE.sh
+++ master.STAR.leafcutter.PE.sh
@@ -9,7 +9,7 @@ do
 
 pwd=$(pwd)
 
-echo "/u/home/s/serghei/code/genomics.hoffman2/run.STAR.leafcutter.sh ${pwd}/${line}_R1_001.fastq.gz ${pwd}/${line}_R2_001.fastq.gz ${line}.STAR.BAM">run.star.leafcutter.${line}.sh
+echo "/u/home/s/serghei/code/genomics.hoffman2/run.STAR.leafcutter.PE.sh ${pwd}/${line}_R1_001.fastq.gz ${pwd}/${line}_R2_001.fastq.gz ${line}.STAR.BAM">run.star.leafcutter.${line}.sh
 #qsub -cwd -V -N map -l h_data=16G,highp,time=24:00:00 run.star.leafcutter.${line}.sh
 
 
