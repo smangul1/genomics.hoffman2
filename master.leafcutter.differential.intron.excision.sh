5274ae6fcd3785bad85f8456c95f839a68eb2237
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-12-21 16:13:15 -0800

    asd

--- master.leafcutter.differential.intron.excision.sh
+++ master.leafcutter.differential.intron.excision.sh
@@ -1,7 +1,22 @@
-# /u/home/s/serghei/code/import/leafcutter/scripts/leafcutter_ds.R --num_threads 4 --exon_file=/u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/gencode.v31.exons.txt.gz /u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/leafcutter_cluster_perind_numers.counts.gz anil_day0_groups_file.txt
+# Rscript /u/home/s/serghei/code/import/leafcutter/scripts/leafcutter_ds.R --num_threads 4 --exon_file=/u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/gencode.v31.exons.txt.gz /u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/leafcutter_cluster_perind_numers.counts.gz anil_day0_groups_file.txt
 
 
-# /u/home/s/serghei/code/import/leafcutter/scripts/leafcutter_ds.R --num_threads 4 --exon_file=/u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/gencode.v31.exons.txt.gz /u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/leafcutter_cluster_perind_numers.counts.gz anil_day2_groups_file.txt
+# Rscript /u/home/s/serghei/code/import/leafcutter/scripts/leafcutter_ds.R --num_threads 4 --exon_file=/u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/gencode.v31.exons.txt.gz /u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/leafcutter_cluster_perind_numers.counts.gz anil_day2_groups_file.txt
 
 
-# /u/home/s/serghei/code/import/leafcutter/scripts/leafcutter_ds.R --num_threads 4 --exon_file=/u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/gencode.v31.exons.txt.gz /u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/leafcutter_cluster_perind_numers.counts.gz anil_day4_groups_file.txt
+# Rscript /u/home/s/serghei/code/import/leafcutter/scripts/leafcutter_ds.R --num_threads 4 --exon_file=/u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/gencode.v31.exons.txt.gz /u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/leafcutter_cluster_perind_numers.counts.gz anil_day4_groups_file.txt
+
+
+
+
+# Rscript /u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/leafcutter/scripts/leafcutter_ds.R --num_threads 4 --exon_file=/u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/gencode.v31.exons.txt.gz /u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/leafcutter_cluster_perind_numers.counts.gz anil_day0_groups_file.txt
+
+
+# Rscript /u/home/s/serghei/code/import/leafcutter/scripts/leafcutter_ds.R --num_threads 4 --exon_file=/u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/gencode.v31.exons.txt.gz /u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/leafcutter_cluster_perind_numers.counts.gz anil_day2_groups_file.txt
+
+
+# Rscript /u/home/s/serghei/code/import/leafcutter/scripts/leafcutter_ds.R --num_threads 4 --exon_file=/u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/gencode.v31.exons.txt.gz /u/home/b/brigitta/scratch/Anil/leafcutter_200_junctions/differential_splicing/leafcutter_cluster_perind_numers.counts.gz anil_day4_groups_file.txt
+
+
+
+
