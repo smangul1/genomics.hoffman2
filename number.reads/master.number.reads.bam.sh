819da3b2abb9ec35c3b1af2794b21909cf17319c
aaronkarlsberg <aaron.karlsberg@gmail.com>
2020-06-30 16:14:22 -0700

    full path to serhgei's home

--- number.reads/master.number.reads.bam.sh
+++ number.reads/master.number.reads.bam.sh
@@ -3,7 +3,7 @@ ls *bam | awk -F ".bam" '{print $1}' >samples.txt
 while read line
 do
 
-echo "~/project/anaconda2/bin/python /u/home/s/serghei/code/miscellaneous.scripts/number.unique.reads.bam/number.reads.bam.py ${line}.bam ${line}.txt">run.${line}.sh
+echo "/u/home/s/serghei/project/anaconda2/bin/python /u/home/s/serghei/code/miscellaneous.scripts/number.unique.reads.bam/number.reads.bam.py ${line}.bam ${line}.txt">run.${line}.sh
 
 #qsub -cwd -V -N  nr -l h_data=8G,highp,time=10:00:00 run.${line}.sh
 
--- number.reads/master.number.reads.gz.sh
+++ number.reads/master.number.reads.gz.sh
@@ -1,4 +1,4 @@
-ls *_R1_001.fastq.gz | awk -F "_R1_001.fastq.gz" '{print $1}' >samples.txti
+ls *_R1_001.fastq.gz | awk -F "_R1_001.fastq.gz" '{print $1}' >samples.txt
 
 
 while read line
