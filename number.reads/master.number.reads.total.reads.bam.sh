78667b0c869512a9e49fb5438664c109ea7609d6
aaronkarlsberg <aaron.karlsberg@gmail.com>
2020-06-30 17:04:55 -0700

    count total reads in bam

---
+++ number.reads/master.number.reads.total.reads.bam.sh
@@ -0,0 +1,18 @@
+
+ls *bam | awk -F ".bam" '{print $1}' >samples.txt
+
+while read line
+do
+
+echo "echo 'total_reads' > ${line}.txt" > run.${line}.sh
+echo "samtools view -c ${line}.bam >> ${line}.txt" >> run.${line}.sh
+
+#qsub -cwd -V -N  nr -l h_data=8G,highp,time=10:00:00 run.${line}.sh
+
+done<samples.txt
+
+
+/u/home/s/serghei/code/miscellaneous.scripts/submit_QSUB_array.sh  4 10
+
+
+
