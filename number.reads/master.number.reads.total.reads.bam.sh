1d3c65cd212af3254937df8211edde9f9153db7b
aaronkarlsberg <aaron.karlsberg@gmail.com>
2020-06-30 17:06:53 -0700

    executable

--- number.reads/master.number.reads.total.reads.bam.sh
+++ number.reads/master.number.reads.total.reads.bam.sh
@@ -4,8 +4,8 @@ ls *bam | awk -F ".bam" '{print $1}' >samples.txt
 while read line
 do
 
-echo "echo 'total_reads' > ${line}.txt" > run.${line}.sh
-echo "samtools view -c ${line}.bam >> ${line}.txt" >> run.${line}.sh
+echo "echo 'total_reads' >> ${line}.txt" > run.total_reads.${line}.sh
+echo "samtools view -c ${line}.bam >> ${line}.txt" >> run.total_reads.${line}.sh
 
 #qsub -cwd -V -N  nr -l h_data=8G,highp,time=10:00:00 run.${line}.sh
 
